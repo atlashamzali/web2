521fb8b8becbd0899d0eb31cdf536123a5e618a6
atlashamzali <ahamzali12159@ada.edu.az>
2024-03-01 11:54:31 +0400

    weeks

---
+++ week02,03,04/first-spring-app/src/main/java/az/edu/ada/wm2/firstspringapp/model/Person.java
@@ -0,0 +1,14 @@
+package az.edu.ada.wm2.firstspringapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person{
+    private String fName;
+    private String lName;
+
+}
